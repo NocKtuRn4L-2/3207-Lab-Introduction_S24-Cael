6fc42b2f3b6944da32e16518812b0afac9644988
dylan Cael <tuq68706@temple.edu>
2024-01-19 19:34:47 -0500

    Finished Header file for function prototype

---
+++ random.h
@@ -0,0 +1,10 @@
+/*
+Dylan Cael - TUQ68706 - 1/17/2024
+CIS-3207-003
+Lab #0 Introduction to git & GitHub
+
+Header file for prototype of the function that calls an already seeded random number generator and returns values from 65-90 to return
+a random character from A-Z
+*/
+
+char randchar() {}
